d05cfb977afd6f4cbf6d67f6b2454fd1f9358a96
myvicosy@gmail.com <myvicosy@gmail.com>
2023-12-10 23:58:07 +0100

    outstanding name showcase feature

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const revealElements = document.querySelectorAll('.hide-element');
-  const revaledCl = 'revealed-element'
+const trenchElements = document.querySelectorAll('.trench')
+const revealElements = document.querySelectorAll('.hide-element')
+const vickoElement = document.getElementById('vicko-')
+const autoHigh = 'high-life'
+const revealedCl = 'revealed-element'
+highChars()
 
-  function revealOnScroll() {
-    const windowHeight = window.innerHeight;
-
-    revealElements.forEach(element => {
-      const revealTop = element.getBoundingClientRect().top;
-
-      if (revealTop < windowHeight / 1.5) {
-        element.classList.add(revaledCl);
-      }
-    });
-
-    // Remove the event listener once all elements are revealed
-    if ([...revealElements].every(element => element.classList.contains(revaledCl))) {
-      window.removeEventListener('scroll', revealOnScroll);
-    }
+async function highChars() {
+  for (const element of trenchElements) {
+    await new Promise(resolve => {
+      element.classList.add(autoHigh)
+      setTimeout(resolve, 50)
+    })
   }
+}
 
-  window.addEventListener('scroll', revealOnScroll);
-});
+setTimeout(() => {
+  vickoElement.style.opacity = '0'
+  setTimeout(() => vickoElement.style.display = 'none' , 1000)
+}, 1500)
+
+
+window.addEventListener('scroll', revealOnScroll);
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+  revealElements.forEach(element => {
+    const revealTop = element.getBoundingClientRect().top;
+    if (revealTop < windowHeight / 1.5) {
+      element.classList.add(revealedCl);
+    }
+  });
+
+  // Remove the event listener once all elements are revealed
+  if ([...revealElements].every(element => element.classList.contains(revealedCl))) {
+    window.removeEventListener('scroll', revealOnScroll);
+  }
+}

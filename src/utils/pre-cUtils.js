e3b5464cc366b30e9e16bc07b8c22f60a049631c
victoryosiobe <myvicosy@gmail.com>
2026-04-25 20:58:57 +0100

    introducing my personal proxy.

--- src/utils/pre-cUtils.js
+++ src/utils/pre-cUtils.js
@@ -1,12 +1,30 @@
-function scrollAndAction(selector, fn, param) {
+function scrollAndAction(selector, fn, param, viewBlock = "start") {
   const element = document.querySelector(selector);
   
-  element.scrollIntoView({
-    behavior: "smooth",
-    block: "start"
-  });
+  if (element) {
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: viewBlock
+    });
+  } else {
+    console.warn(`Element not found: ${selector}`);
+  }
   
   setTimeout(() => fn(param), 500);
 }
 
+function scrollToEl(selector, viewBlock = "start") {
+  const element = document.querySelector(selector);
+  
+  if (element) {
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: viewBlock
+    });
+  } else {
+    console.warn(`Element not found: ${selector}`);
+  }
+}
+
 window.scrollAndAction = scrollAndAction;
+window.scrollToEl = scrollToEl;
